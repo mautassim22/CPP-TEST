04a5a6445f7c81fda27100af26d5e546c0d1e5f9
mautassim <35828480+mautassim@users.noreply.github.com>
2018-03-30 14:44:27 +0200

    Add files via upload

---
+++ musig/main.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    char q,w,e,r,t,y,u,i,o,p;
+
+int ii=1;
+    cout<<"insert"<<endl;
+    cin>>q;
+
+
+    do{
+
+            switch(q){
+
+case 'q':
+
+    PlaySound("q.wav",NULL, SND_FILENAME|SND_SYNC||cin>>w);
+cout<<">>";
+cin>>w;
+
+    break;
+
+    }
+
+      switch(w){
+case 'w':
+     PlaySound("w.wav",NULL, SND_FILENAME|SND_SYNC||cin>>e);
+cout<<">>";
+cin>>e;
+    break;
+
+    }
+
+
+    switch(e){
+case 'e':
+    PlaySound("e.wav",NULL, SND_FILENAME|SND_SYNC||cin>>r);
+cout<<">>";
+cin>>r;
+    break;
+    }
+
+
+    switch(r){
+case 'r':
+    PlaySound("r.wav",NULL, SND_FILENAME|SND_SYNC||cin>>t);
+cout<<">>";
+cin>>t;
+break;
+
+    }
+
+
+    switch(t){
+
+case 't':
+    PlaySound("t.wav",NULL, SND_FILENAME|SND_SYNC||cin>>y);
+cout<<">>";
+cin>>y;
+break;
+
+    }
+
+
+    switch(y){
+
+case 'y':
+    PlaySound("y.wav",NULL, SND_FILENAME|SND_SYNC||cin>>u);
+cout<<">>";
+cin>>u;
+
+    break;
+
+    }
+
+
+    switch(u){
+case 'u':
+
+    PlaySound("u.wav",NULL, SND_FILENAME|SND_SYNC||cin>>i);
+cout<<">>";
+cin>>i;
+    break;
+    }
+
+
+    switch(i){
+case 'i':
+     PlaySound("i.wav",NULL, SND_FILENAME|SND_SYNC||cin>>o);
+cout<<">>";
+cin>>o;
+    break;
+    }
+
+
+    switch(o){
+
+case 'o':
+    PlaySound("o.wav",NULL, SND_FILENAME|SND_SYNC||cin>>p);
+cout<<">>";
+cin>>p;
+    break;
+    }
+
+
+    switch(p){
+case 'a':
+    PlaySound("a.wav",NULL, SND_FILENAME|SND_SYNC);
+break;
+    }
+
+    }while(i<=3009);
+
+
+
+
+
+
+
+    return 0;
+}
